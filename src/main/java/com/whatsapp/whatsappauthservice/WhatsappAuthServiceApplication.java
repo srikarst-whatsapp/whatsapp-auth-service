0eb3364363066026ab0c59ce5176a184be9c1d49
srikarst <srikarsuri@gmail.com>
2024-02-09 17:50:53 +0000

    init

---
+++ src/main/java/com/whatsapp/whatsappauthservice/WhatsappAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.whatsapp.whatsappmessagingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsappAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsappAuthServiceApplication.class, args);
+	}
+
+}
