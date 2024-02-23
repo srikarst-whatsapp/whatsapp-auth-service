ecb884df45e64ba3f3f627f4ecacd53322517c57
srikarst <srikarsuri@gmail.com>
2024-02-23 11:31:51 +0000

    Change port and add welcome route

---
+++ src/main/java/com/whatsapp/whatsappauthservice/web/MainController.java
@@ -0,0 +1,29 @@
+package com.whatsapp.whatsappauthservice.web;
+
+import jakarta.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.whatsapp.whatsappauthservice.entity.User;
+import com.whatsapp.whatsappauthservice.service.UserService;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("")
+public class MainController {
+
+    @GetMapping("")
+    public ResponseEntity<String> sayWelcome() {
+        return new ResponseEntity<>("Hi and welcome", HttpStatus.CREATED);
+    }
+
+}
